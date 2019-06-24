09f43937d5d35a043708e3ba59ef81a9e881f333
AdeonMaster <adeonmaster@gmail.com>
2019-06-24 12:48:39 +0300

    Removed redux-logger

--- src/main.js
+++ src/main.js
@@ -9,26 +9,18 @@ import { Provider } from 'react-redux';
 
 import thunkMiddleware from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
-import { createLogger as createLoggerMiddleware } from 'redux-logger';
 
 import rootReducer from './redux/rootReducer';
 import rootSaga from './redux/rootSagas';
 import App from './components/App';
 import './scss/main.scss';
 
-const loggerMiddleware = createLoggerMiddleware();
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = process.env.NODE_ENV === 'development'
-  ? [
-    thunkMiddleware,
-    sagaMiddleware,
-    loggerMiddleware
-  ]
-  : [
-    thunkMiddleware,
-    sagaMiddleware
-  ];
+const middlewares = [
+  thunkMiddleware,
+  sagaMiddleware
+];
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
