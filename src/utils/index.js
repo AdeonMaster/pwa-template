35d81e2c5bdf7806df954c8590931a46e3eb669c
AdeonMaster <adeonmaster@gmail.com>
2019-06-24 11:50:52 +0300

    Minor changes

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-export const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
-
-export const buildEnum = (prefix, keys) => keys.reduce((obj, key) => {
-  // eslint-disable-next-line no-param-reassign
-  obj[key] = `${prefix}_${key}`;
-
-  return obj;
-}, {});
--- src/utils/request/constants.js
+++
@@ -1,6 +0,0 @@
-export const REQUEST = {
-  API_URL: 'https://api.cryptogateways.io',
-  MOCK_API_URL: 'http://localhost',
-  SLEEP: 0,
-  USE_MOCKS: false
-};
