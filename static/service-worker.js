34b68757ecdf7b89d2e8ad71e619f744dfdb827c
AdeonMaster <adeonmaster@gmail.com>
2019-09-03 16:49:38 +0300

    Minor changes

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,12 +1,24 @@
-// Set this to true for production
-var doCache = true;
-
-// Name our cache
+var productionMode = true;
 var CACHE_NAME = 'pwa-template-cache-v1';
 
+// The first time the user starts up the PWA, 'install' is triggered.
+self.addEventListener('install', function(event) {
+  if (productionMode) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/img/favicon.ico'
+        ]);
+      })
+    );
+  }
+});
+
 // Delete old caches that are not our current one!
-self.addEventListener("activate", event => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener("activate", function(event) {
+  var cacheWhitelist = [CACHE_NAME];
 
   event.waitUntil(
     caches.keys().then(keyList =>
@@ -21,25 +33,9 @@ self.addEventListener("activate", event => {
   );
 });
 
-// The first time the user starts up the PWA, 'install' is triggered.
-self.addEventListener('install', function(event) {
-  if (doCache) {
-    event.waitUntil(
-      caches.open(CACHE_NAME).then(function(cache) {
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/img/favicon.ico'
-        ]);
-      })
-    );
-  }
-});
-
 // When the webpage goes to fetch files, we intercept that request and serve up the matching files
-// if we have them
 self.addEventListener('fetch', function(event) {
-  if (doCache) {
+  if (productionMode) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
