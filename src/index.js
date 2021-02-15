6fcc8c0ebd3ce3dde85eaa031598612948be70ce
AdeonMaster <adeonmaster@gmail.com>
2021-02-15 20:22:03 +0200

    Added better error handling

--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 import App from './app';
-// import { SENTRY_DNS } from './common/constants';
+// import { SENTRY_DNS, APP_ID } from './common/constants';
 import ErrorBoundary from './common/components/error-boundary';
 // import { isLocalhost } from './common/utils';
+// import { getLang } from './common/selectors/app-selectors';
 import './common/scss/main.scss';
 
 // if (!isLocalhost()) {
@@ -24,23 +25,35 @@ import './common/scss/main.scss';
 //   // for finer control
 //   tracesSampleRate: 1.0,
 // });
+
+// Sentry.setContext('Additional Details', {
+//   app: APP_ID,
+//   version: process.env.PACKAGE_VERSION,
+//   lang: getLang(store),
+// });
 // }
 
 // call specific redux action on service worker updatefound event
 if (window?.navigator?.serviceWorker?.getRegistrations) {
-  window.navigator.serviceWorker.getRegistrations().then((registrations) => {
-    const [registration] = registrations;
+  window.navigator.serviceWorker
+    .getRegistrations()
+    .then((registrations) => {
+      const [registration] = registrations;
 
-    if (registration) {
-      registration.addEventListener('updatefound', (event) => {
-        if (event.currentTarget.active) {
-          store.dispatch({
-            type: 'sw/updated',
-          });
-        }
-      });
-    }
-  });
+      if (registration) {
+        registration.addEventListener('updatefound', (event) => {
+          if (event.currentTarget.active) {
+            store.dispatch({
+              type: 'sw/updated',
+            });
+          }
+        });
+      }
+    })
+    .catch((getRegistrationsError) => {
+      // eslint-disable-next-line no-console
+      console.log('SW getRegistrations failed: ', getRegistrationsError);
+    });
 }
 
 ReactDOM.render(
